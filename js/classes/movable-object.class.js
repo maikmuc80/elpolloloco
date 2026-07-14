f2b8c3155d7d24e9c47b599cea502d15e9214ff0
Maik Radke <maikmuc80@gmail.com>
2026-07-14 16:52:09 +0200

    fix: correct hitbox offsets in collision detection

--- js/classes/movable-object.class.js
+++ js/classes/movable-object.class.js
@@ -63,10 +63,10 @@ class MovableObject extends DrawableObject {
      * @returns {boolean} True if both hitboxes overlap.
      */
     isColliding(mo) {
-        return this.x + this.offset.left + this.width - this.offset.right > mo.x + mo.offset.left &&
-            this.y + this.offset.top + this.height - this.offset.bottom > mo.y + mo.offset.top &&
-            this.x + this.offset.left < mo.x + mo.offset.left + mo.width - mo.offset.right &&
-            this.y + this.offset.top < mo.y + mo.offset.top + mo.height - mo.offset.bottom;
+        return this.x + this.width - this.offset.right > mo.x + mo.offset.left &&
+            this.y + this.height - this.offset.bottom > mo.y + mo.offset.top &&
+            this.x + this.offset.left < mo.x + mo.width - mo.offset.right &&
+            this.y + this.offset.top < mo.y + mo.height - mo.offset.bottom;
     }
 
     /** Reduces energy on hit and stores the time of the hit. */
